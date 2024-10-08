68d925677eb5d307788e32ff2bcf2ca5ded0c0f1
Jun Hashimoto <2000jun11@gmail.com>
2024-10-08 18:05:20 +0900

    問題数の変更を行いました。

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+//問題文と答えをまとめる構造体
+struct  Question
+{
+	string s; //問題文
+	int a; //答え
+};
+
 int main()
 {
+	Question questions[] = {
+		{"13*-5",13 * -5},
+		{"(-21)÷(-3)",-21 / -3},
+		{"7-(4+2)÷2", 7 - (4 + 2) / 2},
+	};
+
 	cout << "[リクルート試験対策クイズ]\n";
 
-	cout << "13x(-5)の答えは?\n";
-
-	int answer;
-
-	cin >> answer;
-
-	if (answer == 13 * -5)
+	
+	for (const auto& e : questions)
 	{
-		cout << "正解!\n";
-	}
-	else
-	{
-		cout << "間違い!正解は" << 13 * -5 << "\n";
-	}
+		cout << e.s << "の答えは?\n";
+		int answer;
+
+		cin >> answer;
+
+		if (answer == e.a)
+		{
+			cout << "正解!\n";
+		}
+		else
+		{
+			cout << "間違い!正解は" << e.a << "\n";
+		}
+	}//for questions
+
 }
